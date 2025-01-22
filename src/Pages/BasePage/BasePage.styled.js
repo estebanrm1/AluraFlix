a64495687260b87fbb1831a31846472a65cea4fd
Esteban Molina <solsel90@gmail.com>
2025-01-22 00:35:25 -0300

    Se modifico el color de fonde de la pagina base

--- src/Pages/BasePage/BasePage.styled.js
+++ src/Pages/BasePage/BasePage.styled.js
@@ -4,6 +4,7 @@ export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  background-color: black;
 `;
 
 export const ContentWrapper = styled.div`
